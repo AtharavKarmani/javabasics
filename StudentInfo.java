bcc6e387ae4efc93e3a2593a99dcb59f00842c4b
AtharavKarmani <AtharavKarmani>
2026-02-10 15:22:40 +0530

    initial commit

---
+++ StudentInfo.java
@@ -0,0 +1,15 @@
+public class StudentInfo{
+public static void main(String[] args){
+// Basic student details 
+String name ="ATHARAV KARMANI";
+int year = 1;
+String branch = "CSE";
+double cgpa = 9.32;
+boolean isHosteller = false;
+System.out.println("=== Info ===");
+System.out.println("Name:" +name);
+System.out.println("Year:" +year);
+System.out.println("Branch:"+branch);
+System.out.println("CGPA: " +cgpa);
+}
+}
---
+++ variablesDemo.java
@@ -0,0 +1,11 @@
+public class variablesDemo {
+	public static void main(String[] args){
+		// Integer variable 
+		int age = 18;
+		// Decimal variable 
+		double price = 99.50;
+		// Character & Boolean 
+		char grade ='A';
+		boolean isPassed = true;
+	}
+}
